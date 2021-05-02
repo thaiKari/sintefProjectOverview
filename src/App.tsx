44364d691da4d627af914a8baef9c7abd1341625
Kari.Meling.Johannessen <karjoh@norkart.no>
2021-05-02 13:22:52 +0200

    use mapbox tiles

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import { MapContainer, TileLayer} from 'react-leaflet'
 function App() {
   return (
 
-      <MapContainer style={{ width: '100vw', height: '100vh' }} center={[51.505, -0.09]} zoom={13}>
+      <MapContainer style={{ width: '100vw', height: '100vh' }} center={[30,0]} zoom={3}>
         <TileLayer
+        id='map'
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=${process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}`}
         />
       </MapContainer>
   );
