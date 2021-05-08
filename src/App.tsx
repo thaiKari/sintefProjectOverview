038811ef778ab2dda3b554ccf8e81c5585118335
Kari.Meling.Johannessen <karjoh@norkart.no>
2021-05-08 21:49:42 +0200

    chloropleth colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Polygon, Tooltip } from 'react-leaflet'
-import {Typography} from '@material-ui/core/'
+import { Container, Typography } from '@material-ui/core/'
 import { Data, data } from './data/data';
 import countries from './data/countries.json'
+import { LatLngBoundsLiteral } from 'leaflet';
+import { InfoBox } from './InfoBox';
 
 function swapLatLn(coords: any[]): any[] {
   if (coords.length === 2) {
@@ -35,8 +37,23 @@ const mapData = (): Data[] => {
 
 }
 
-const getColor = (data: Data) => {
-  return 'purple'
+const worldBounds: LatLngBoundsLiteral = [
+  [-60, -180],
+  [80, 192],
+]
+
+const colors: string[] = ["#298e2c", "#42983f", "#6bac63", "#99c692", "#bddab7", "#cce3c8"]
+
+const stops: number[] = [3, 6, 12, 24, 48]
+
+const getColor = (value: number): string => {
+  for (let i = 0; i < stops.length; i++) {
+    if (value < stops[i]) {
+      return colors[i]
+    }
+  }
+
+  return colors[colors.length - 1]
 }
 
 function App() {
@@ -44,51 +61,61 @@ function App() {
   const [selected, setselected] = useState<Data | null>(null)
 
   useEffect(() => {
+    console.log(colors.reverse())
     setprocessedData(mapData())
   }, [])
 
-  function getPathOptions(data:Data): import("leaflet").PathOptions | undefined {
+  function getPathOptions(data: Data): import("leaflet").PathOptions | undefined {
     const isSelected = selected && selected.Id === data.Id
-    const color = getColor(data)
+    const color = getColor(data.Samples)
 
-    return { color: isSelected ? color: 'transparent', fillColor: color, fillOpacity:0.6 }
+    return { color: isSelected ? 'gray' : 'transparent', fillColor: color, fillOpacity: 0.6 }
 
   }
-  
+
 
   return (
-    <MapContainer style={{ width: '100vw', height: '100vh' }} center={[30, 0]} zoom={3}>
-      <TileLayer
-        id='map'
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url={`https://api.mapbox.com/styles/v1/karimj/cjm203zaw16gb2ro02gfsmt3l/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}`}
-      />
+    <Container style={{ display: 'flex', height: '100vh', alignItems: 'center' }}>
 
-      {processedData && processedData.map(data =>
-        <Polygon
-          pathOptions={getPathOptions(data)}
-          positions={data.coordinates ?? []}
-          eventHandlers={{
-            mouseover: () => {setselected(data)},
-            mouseout: () => {setselected(null)}
-          }}
-        >
-          <Tooltip sticky>
-          <Typography variant="h5" component="h2">
-            {data.Name }
-          </Typography>
-          <Typography color="textSecondary">
-            {`${data.Assignments} Assignments`}
-          </Typography>
-          </Tooltip>
-        </Polygon>
-      )}
+      <MapContainer style={{ width: '100%', height: 700 }} bounds={worldBounds}>
 
-    </MapContainer>
+        <InfoBox data={selected} />
+        <TileLayer
+          id='map'
+          url={`https://api.mapbox.com/styles/v1/karimj/cjm203zaw16gb2ro02gfsmt3l/tiles/256/{z}/{x}/{y}?access_token=${process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}`}
+        />
+
+        {processedData && processedData.map(data =>
+          <Polygon
+            key={data.Id}
+            pathOptions={getPathOptions(data)}
+            positions={data.coordinates ?? []}
+            eventHandlers={{
+              mouseover: () => { setselected(data) },
+              mouseout: () => { setselected(null) }
+            }}
+          >
+            <Tooltip sticky>
+              <Typography variant="h5" component="h2">
+                {data.Name}
+              </Typography>
+              <Typography color="textSecondary">
+                {`${data.Assignments} Assignments`}
+              </Typography>
+            </Tooltip>
+          </Polygon>
+        )}
+
+      </MapContainer>
+
+    </Container>
+
   );
 }
 
 export default App;
+
+
 
 
 
